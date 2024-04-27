53349547c446011822f37dcb1b049f407567af64
Ariel Denaro <arieldenaro@gmail.com>
2024-04-27 16:47:59 -0300

    Formulario terminado - falta revision diseño

--- scripts/validateForm.js
+++ scripts/validateForm.js
@@ -18,5 +18,29 @@ function validateForm() {
         alert("Por favor deje un mensaje.");
         return false;
     }
+
+
+    // Limpiar campos del formulario
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("provincia").selectedIndex = 0; //Valor por defecto en el dropdown
+    document.getElementById("mensaje").value = "";
+
+    showPopup(name);
+    
     return true;
+}
+
+function showPopup(nombreUsuario) {
+    var popup = document.getElementById("popup");
+    var popupTitle = document.querySelector(".popup-title");
+    var popupMessage = document.querySelector(".popup-message");
+    popupTitle.textContent = "¡Hola, " + nombreUsuario + "!";
+    popupMessage.textContent = "Gracias por completar el formulario de contacto.\n Nos vamos a poner en contacto para responder a tu consulta.";
+    popup.style.display = "block";
+}
+
+function closePopup() {
+    var popup = document.getElementById("popup");
+    popup.style.display = "none";
 }
